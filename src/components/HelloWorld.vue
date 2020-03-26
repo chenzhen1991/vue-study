a8e96d02dc26ea4b24298cddccdc846062441ab4
czw0919@163.com <czw0919@163.com>
2020-03-26 14:48:28 +0800

    feat vue-vuex手写

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,24 @@
 <template>
   <div>
-    <button @click="handle">点我点我</button>
-    <div>{{add}}</div>
-    <p>{{$attrs.refresh}}</p>
-<!--    <p>{{pro}}</p>-->
-    <div>
-      插槽
-      <slot></slot>
-      <slot name='nameSlot'></slot>
-      <slot name='name1' :foo='foo'></slot>
-    </div>
+<!--    <button @click="handle">点我点我</button>-->
+<!--    <div>{{add}}</div>-->
+<!--    <p>{{$attrs.refresh}}</p>-->
+<!--&lt;!&ndash;    <p>{{pro}}</p>&ndash;&gt;-->
+<!--    <div>-->
+<!--      插槽-->
+<!--      <slot></slot>-->
+<!--      <slot name='nameSlot'></slot>-->
+<!--      <slot name='name1' :foo='foo'></slot>-->
+<!--    </div>-->
+    <p @click="$store.commit('add')">counter: {{$store.state.counter}}</p>
+    <p @click="$store.dispatch('add')">async counter: {{$store.state.counter}}</p>
+<!--    <p>double {{$store.getters.doubleCount}}</p>-->
   </div>
 </template>
 
 <script>
   export default {
-    name:'HelloWorld',
-    data() {
-      return {
-        child: 'value',
-        add:'',
-        foo:'插槽传递过来的参数'
-      }
-    },
-    // inject:{
-    //   pro:'pro'
-    // },
-    mounted(){
-      this.$parent.$on('foo', (msg) => {
-        console.log('msg'+msg)
-      })
-    },
-    methods: {
-      handle() {
-        this.$parent.$emit('foo','23444')
-      }
-    },
+    name:'HelloWorld'
   }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import create from './utils/create';
 import router from './router'
-console.dir(create);
+import store from './store'
+console.dir(store);
 
 Vue.config.productionTip = false
 Vue.prototype.$create = create
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
