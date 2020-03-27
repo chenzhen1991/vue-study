18325da1111751609bd9dd0d04ed7f890d6cf9c2
czw0919@163.com <czw0919@163.com>
2020-03-27 17:53:50 +0800

    feat vuex rewrite

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import create from './utils/create';
 import router from './router'
 import store from './store'
 console.dir(store);
 
 Vue.config.productionTip = false
-Vue.prototype.$create = create
 new Vue({
   router,
   store,
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from './zVuex'
+console.log(Vuex)
 
 Vue.use(Vuex)
 
